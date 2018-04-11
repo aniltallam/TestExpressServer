15b937c465d188f0e34f626ed605292904d2aa6b
Anil Tallam <aniltallam@gmail.com>
2018-04-11 13:48:18 +0530

    more changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var Express = require('express')
 var bodyParser = require('body-parser')
-
+let multer = require('multer')
+let upload = multer({ storage: multer.memoryStorage() })
 var app = new Express()
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
-
 
 app.get('/test', function (req, res) {
   var data = {
@@ -16,12 +16,38 @@ app.get('/test', function (req, res) {
 
 app.get('/samplejson', function (req, res) {
   var data = [
-    {key1: 'val1'},
-    {key1: 'val2'},
-    {key1: 'val3'}
+    {key1: 'val1', id: 1, Name: 'anil', "Display Name": 'hello'}
+    //{key1: 'val2'},
+    //{key1: 'val3'}
   ]
   console.log('h=/samplejson, sendingData=', JSON.stringify(data))
+  //return server.close(function () {})
   res.json(data)
+})
+
+app.get('/largeData', function (req, res) {
+  var data = []
+  for (let i=0; i< 2900; i++) {
+	  let record = { k: i, v: 'v' + i}
+	  data.push(record)
+  }
+  console.log('h=/largeData, sendingData=', JSON.stringify(data))
+  res.json(data)
+})
+
+app.get('/invalidXml', function (req, res) {
+  // var data = '<?xml version="1.0" encoding="utf-8"?><customer><name>1</name></customer>'
+  var data = '<?xml version="1.0" encoding="utf-8"?>'
+  console.log('h=/invalidXml, sendingData=', data)
+  res.set('Content-Type', 'text/xml')
+  res.send(data)
+})
+
+app.get('/redirectMe', function (req, res) {
+  console.log('h=/redirectMe')
+  // res.redirect('/samplejson')
+  // res.redirect('http://ab06cfa8.ngrok.io/samplejson')
+  res.redirect('/redirectMe')
 })
 
 app.post('/mirror', function (req, res) {
@@ -31,15 +57,61 @@ app.post('/mirror', function (req, res) {
 
 app.post('/print', function (req, res) {
   console.log('h=/print, m=post, body=', JSON.stringify(req.body))
-  res.status(204)
+  // res.status(204)
+  res.status(204).end()
 })
 
-app.get('*', function (req, res) {
-  console.log('h=/Undefined Route=>', req.url, 'headers=', req.headers)
-  res.status(404).json({ errors: [new Error('SERVER_RESOURCE_NOT_FOUND ' + req.url)] })
+app.post('/v1/products/:name', function (req, res) {
+  console.log('h=/v1/products/, m=post, body=', JSON.stringify(req.body))
+  // res.status(204)
+  res.status(204).end()
 })
 
-app.listen(5050, function (err) {
+app.put('/v1/products/:name', function (req, res) {
+  console.log('h=/v1/products/, m=put, body=', JSON.stringify(req.body))
+  // res.status(204)
+  res.status(204).end()
+})
+
+app.post('/printm', upload.array('somefile'), function (req, res) {
+  // console.log('h=/print, m=post, body=', JSON.stringify(req.body))
+  // res.status(204)
+  console.log(req.body)
+  console.log(req.files)
+  req.files.forEach((f) => {
+    console.log(f.buffer.toString())
+  })
+  // res.send()
+  res.json('ended fine')
+})
+
+app.post('/raw', (req, res) => {
+
+  // output the headers
+  console.log(req.headers);
+
+  // capture the encoded form data
+  req.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  // send a response when finished reading
+  // the encoded form data
+  req.on('end', () => {
+    res.send('ok');
+  });
+});
+
+// app.get('*', function (req, res) {
+//   console.log('h=/Undefined Route=>', req.url, 'headers=', req.headers)
+//   res.status(404).json({ errors: [new Error('SERVER_RESOURCE_NOT_FOUND ' + req.url)] })
+// })
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!" + req.url + req.method)
+})
+
+const server = app.listen(5050, function (err) {
   if (err) return console.log('!!!!!!!!!!!! Server was down with Error =======>', err)
   else console.log('Server started on port =', 5050)
 })
